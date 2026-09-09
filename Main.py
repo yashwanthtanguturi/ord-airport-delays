eb7f5af0c1893e8fed07a50aa7c4018d604c877c
yashwanthtanguturi <yashwanth.t08@outlook.com>
2026-09-09 14:48:21 -0400

    Added the pandas library to the file and loaded data into dataframe. Cleaning and filtering data

--- Main.py
+++ Main.py
@@ -1,7 +1,17 @@
 import pandas as pd
 
+
+# making the dataframe for the data
 airport_df = pd.read_csv('data/ORD_flights_june2026.csv')
 
 print(airport_df.shape)
 print(airport_df.head())
 print(airport_df['ORIGIN_AIRPORT_ID'].unique()[:10])
+
+
+# filtering and cleaning the data up
+cols = ['FL_DATE', 'OP_CARRIER', 'DEP_DELAY', 'ORIGIN_AIRPORT_ID', 'DEST_AIRPORT_ID']
+airport_df = airport_df[cols]
+airport_df = airport_df.dropna(subset=['DEP_DELAY'])
+
+print(airport_df.shape)
